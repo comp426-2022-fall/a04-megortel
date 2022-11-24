add6dd81bb07950ce77e949984d102f43ec8d831
root <root@DESKTOP-2HT3G61.localdomain>
2022-11-24 03:21:38 -0500

    working

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
+import minimist from "minimist"
+import express from "express"
+import roll from "./lib/roll.js"
 
+const app = express();
+
+const args = minmist(process.argv.slice(2));
+
+app.arguments(express.json());
+app.arguments(express.urlencoded({extended:true}));
+let port = 5000;
+
+if(args.port){
+    port = args.port;
+}
+
+app.get('/app/', (req, res) => {
+    res.status(200);
+    res.send("200 OK");
+});
+
+app.get('/app/roll/', (req, res) => {
+    res.send(roll(6, 3, 1));
+});
+
+app.get('/app/roll/', (req, res, next) => {
+    let sides = parseInt(req.body.sides);
+    let dice = parseInt(req.body.dice);
+    let rolls = parseInt(req.body.rolls);
+    res.send(roll(sides, dice, rolls)).end();
+});
+
+app.get('/app/roll/:sides/', (req, res, next) => {
+    let sides = parseInt(req.params.sides);
+    res.send(roll(sides, 3, 1)).end();
+});
+
+app.get('/app/roll/:sides/', (req, res, next) => {
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    res.send(roll(sides, dice, 1)).end();
+});
+
+app.get('/app/roll/:sides/', (req, res, next) => {
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = parseInt(req.params.rolls);
+    res.send(roll(sides, dice, rolls)).end();
+});
+
+app.use((req, res) =>{
+    res.status(404).send("404 NOT FOUND").end();
+});
+
+app.listen(port);
